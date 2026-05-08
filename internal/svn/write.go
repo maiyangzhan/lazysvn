7848afa2ac7111e8ecb4fa9d0f88e19f9d8ae702
maiyangzhan <maiyangzhan@outlook.com>
2026-05-08 22:35:46 +0800

    feat(svn): Commit/Revert/Add/Remove/Resolved

---
+++ internal/svn/write.go
@@ -0,0 +1,31 @@
+package svn
+
+func (c *Client) Commit(paths []string, msg string) error {
+	args := append([]string{"commit", "-m", msg}, paths...)
+	_, err := c.run(args...)
+	return err
+}
+
+func (c *Client) Revert(paths []string) error {
+	args := append([]string{"revert"}, paths...)
+	_, err := c.run(args...)
+	return err
+}
+
+func (c *Client) Add(paths []string) error {
+	args := append([]string{"add"}, paths...)
+	_, err := c.run(args...)
+	return err
+}
+
+func (c *Client) Remove(paths []string) error {
+	args := append([]string{"rm"}, paths...)
+	_, err := c.run(args...)
+	return err
+}
+
+func (c *Client) Resolved(paths []string) error {
+	args := append([]string{"resolved"}, paths...)
+	_, err := c.run(args...)
+	return err
+}
